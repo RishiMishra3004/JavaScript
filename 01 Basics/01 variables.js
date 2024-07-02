c446f0525f17871b13fbe118003c441eb37a3313
RishiMishra3004 <12rishi2003@gmail.com>
2024-07-02 21:51:56 +0530

    datatypes

---
+++ 01 Basics/01 variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountemail = "jlanfbl@email.com"
+var pass = "12345"
+city = "allahabad"
+let accountstate;
+accountemail = "skjnf@.com"
+// console.log(city);
+
+/*
+prefer not use var 
+beacuse of issue in block scpe and funtional scope 
+*/
+
+console.table([accountId, accountemail, pass, city, accountstate])
