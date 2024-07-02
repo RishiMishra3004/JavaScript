5ca57f348daa52a2a2d9d189e607ac724ea2dc0c
RishiMishra3004 <12rishi2003@gmail.com>
2024-07-02 22:03:20 +0530

    Conversion of datatypes

---
+++ 01 Basics/03 ConversionOperations.js
@@ -0,0 +1,23 @@
+let score = true
+// console.log(typeof score);
+
+let value = Number(score)
+// console.log(typeof value);
+// console.log(value);
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1
+
+let isLoggedIn = "fafa"
+
+let booleanloggedIn = Boolean(isLoggedIn)
+console.log(booleanloggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+
+let num = 33;
+
+let stringnum = String(num);
+console.log(typeof stringnum);
